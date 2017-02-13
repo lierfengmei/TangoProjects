4066431dd595a34452de8df712af96a3a53ca734
lierfengmei <lifengmei89@sina.com>
2017-02-13 11:11:34 +0800

    chapter6.2 showing Categories on Rango's Homepage

--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
+
+
+#def index(request):
+#    return HttpResponse('Rango still  says here is the about page')
 
 def index(request):
-    return HttpResponse('Rango still  says here is the about page')
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in decending order.
+    # Retrieve the top 5 only - or all if less than 5
+    # Place the list in the context_dict dictionary
+    # that will be passed to the template engine
 
-def index_name(request):
-    context_dict = {'variable_name':'Lifengmei','boldmessage':"Crunchy,creamy,cookie,candy,cupcake",}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories':category_list}
+
+
+    # Render the response and send it back
     return render(request,'rango/index.html',context_dict)
+
+
+# def index_name(request):
+ #   context_dict = {'variable_name':'Lifengmei','boldmessage':"Crunchy,creamy,cookie,candy,cupcake",}
+ #   return render(request,'rango/index.html',context_dict)
+
 
 def about(request):
     return render(request,'rango/about.html',)
