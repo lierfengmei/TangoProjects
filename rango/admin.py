e10b67c3f79af615dd07b15da53189a8870a0844
lierfengmei <lifengmei89@sina.com>
2017-02-13 10:00:10 +0800

    add url and category of page_show, add tests.py

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Category,Page
+#from admin import ModelAdmin
 
 admin.site.register(Category)
-admin.site.register(Page)
+#admin.site.register(Page)
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title','category','url')
+
+admin.site.register(Page,PageAdmin)
 
